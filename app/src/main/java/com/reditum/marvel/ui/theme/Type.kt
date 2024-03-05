2cc75f51983067114f95662d47595b9b1f51d02f
Red1tum <fantomskn@gmail.com>
2024-03-05 15:35:05 +0600

    add fonts to typography

--- app/src/main/java/com/reditum/marvel/ui/theme/Type.kt
+++ app/src/main/java/com/reditum/marvel/ui/theme/Type.kt
@@ -2,33 +2,38 @@ package com.reditum.marvel.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.reditum.marvel.R
 
-// Set of Material typography styles to start with
+val libreFonts = FontFamily(
+    Font(resId = R.font.libre_franklin_bold, FontWeight.Bold),
+    Font(resId = R.font.libre_franklin_medium, FontWeight.Medium),
+    Font(resId = R.font.libre_franklin_medium, FontWeight.Normal)
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = libreFonts,
+        fontSize = 36.sp,
+        lineHeight = 36.sp,
         letterSpacing = 0.5.sp
-    )
-    */
+    ),
+    titleMedium = TextStyle(
+        fontWeight = FontWeight.Medium,
+        fontFamily = libreFonts,
+        fontSize = 32.sp,
+        lineHeight = 36.sp,
+        letterSpacing = 0.5.sp
+    ),
+    bodyMedium = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontFamily = libreFonts,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.5.sp
+    ),
 )
